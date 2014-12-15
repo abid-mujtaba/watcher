c794c6c8b9fd77027c279d586e3d801a40adc24b
Abid H Mujtaba <abid.naqvi83@gmail.com>
2014-12-15 19:22:51 +0500

    Daemonizes the script.

--- watcher.py
+++ watcher.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 # Author: Abid H. Mujtaba
 # Date: 2014-12-15
@@ -9,7 +9,11 @@
 
 import pyinotify
 import subprocess
+import sys
 import syslog
+
+from daemon import Daemon
+
 
 # Import the flags that we will use to monitor events in the specified folder
 IN_CLOSE_WRITE = pyinotify.EventsCodes.FLAG_COLLECTIONS['OP_FLAGS']['IN_CLOSE_WRITE']
@@ -25,6 +29,9 @@ MONITOR_FOLDERS = ['/home/abid/Documents/workspace/.misc',
 # We specify the shell script that has to be run when the specified flags are triggered in the specified folders
 TRIGGERED_SCRIPT = "/home/abid/bin/sanitize_recent_docs"
 
+# Specify the pid file used to monitor and control the daemon/service.
+PID_FILE = "/tmp/watcher.pid"
+
 
 class MyEventHandler(pyinotify.ProcessEvent):
 
@@ -38,26 +45,64 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
     def on_close(self, flag, pathname):
 
-        print "{}  - {}".format(flag, pathname)
         syslog.syslog("{} - {}".format(flag, pathname))
 
         subprocess.call([TRIGGERED_SCRIPT])
 
 
+class WatcherDaemon(Daemon):
+    """
+    Extends the Daemon class by overriding its run() method which provides the primary functionality provided by the
+    service.
+
+    NOTE the loop used in the end.
+    """
+
+    def run(self):
+        # watch manager
+        wm = pyinotify.WatchManager()
+
+        for folder in MONITOR_FOLDERS:
+
+            wm.add_watch(folder, MONITOR_FLAGS, rec=True)
+
+        # event handler
+        eh = MyEventHandler()
+
+        # notifier
+        notifier = pyinotify.Notifier(wm, eh)
+        notifier.loop()
+
+
 def main():
-    # watch manager
-    wm = pyinotify.WatchManager()
+    """
+    This method sets up the daemon and passes the command to it.
 
-    for folder in MONITOR_FOLDERS:
+    Source: http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+    """
 
-        wm.add_watch(folder, MONITOR_FLAGS, rec=True)
+    daemon = WatcherDaemon(PID_FILE)        # The pid file must be specified so that a running daemon can be accessed
 
-    # event handler
-    eh = MyEventHandler()
+    if len(sys.argv) == 2:
 
-    # notifier
-    notifier = pyinotify.Notifier(wm, eh)
-    notifier.loop()
+        if 'start' == sys.argv[1]:
+                daemon.start()
+
+        elif 'stop' == sys.argv[1]:
+                daemon.stop()
+
+        elif 'restart' == sys.argv[1]:
+                daemon.restart()
+
+        else:
+                print "Unknown command"
+                sys.exit(2)
+
+        sys.exit(0)         # We have communicated with the daemon which runs asynchronously so we can exit this script
+
+    else:
+        print "Usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
 
 
 if __name__ == '__main__':
